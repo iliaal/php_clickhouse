135d694dae22e801247f017f710cc8b4294c8604
Ilia Alshanetsky <ilia@ilia.ws>
2026-05-11 11:40:52 -0400

    Release 0.8.5

--- php_clickhouse.h
+++ php_clickhouse.h
@@ -26,7 +26,7 @@
 extern zend_module_entry clickhouse_module_entry;
 #define phpext_clickhouse_ptr &clickhouse_module_entry
 
-#define PHP_CLICKHOUSE_VERSION "0.8.1"
+#define PHP_CLICKHOUSE_VERSION "0.8.5"
 
 #ifdef PHP_WIN32
 #	define PHP_CLICKHOUSE_API __declspec(dllexport)
